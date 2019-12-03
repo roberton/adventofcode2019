676e438e47aca5845c2013b0ec801d7cebe001d0
roberton <rnerici@gmail.com>
2019-12-03 17:12:54 +0000

    calculates second star of day 01

--- src/01.ts
+++ src/01.ts
@@ -3,23 +3,41 @@ import * as util from 'util'
 const readFile = util.promisify(fs.readFile)
 import { forStatement } from "@babel/types"
 
-export function calcFuelForModule(mass: number): number {
-  return Math.floor(mass / 3) - 2
+export function calcFuel(mass: number): number {
+  return Math.max(Math.floor(mass / 3) - 2, 0)
 }
 
-export function calcSumOfFuelRequirements(moduleMasses: number[]): number {
+export function calcFuelIncludingItsFuel(mass: number): number {
+  if (mass === 0) return 0
+  const fuel = calcFuel(mass)
+  return fuel + calcFuelIncludingItsFuel(fuel)
+}
+
+export function calcSumOfFuelRequirements(moduleMasses: number[], calcFun: (mass: number) => number): number {
   return moduleMasses.reduce((sum, moduleMass) => {
-    return sum + calcFuelForModule(moduleMass)
+    return sum + calcFun(moduleMass)
   }, 0)
 }
 
-part1()
+async function readModuleMasses() {
+  const fileContents = await readFile('data/01.txt', 'utf-8');
+  const lines = fileContents.split('\n');
+  const moduleMasses = lines.map(line => parseInt(line, 10));
+  return moduleMasses;
+}
 
 async function part1() {
-  const fileContents = await readFile('data/01.txt', 'utf-8')
-  console.log(fileContents)
-  const lines = fileContents.split('\n')
-  const moduleMasses = lines.map(line => parseInt(line, 10))
-  const fuel = calcSumOfFuelRequirements(moduleMasses)
+  const moduleMasses = await readModuleMasses();
+  const fuel = calcSumOfFuelRequirements(moduleMasses, calcFuel)
   console.log(fuel)
 }
+
+async function part2() {
+  const moduleMasses = await readModuleMasses();
+  const fuel = calcSumOfFuelRequirements(moduleMasses, calcFuelIncludingItsFuel)
+  console.log(fuel)
+}
+
+part1()
+part2()
+
--- test/01.test.ts
+++ test/01.test.ts
@@ -1,31 +1,58 @@
-import {calcFuelForModule, calcSumOfFuelRequirements} from '../src/01';
+import {calcFuel, calcSumOfFuelRequirements, calcFuelIncludingItsFuel} from '../src/01';
 
 describe('the fuel for a module', () => {
   it('of mass 12 should be 2', () => {
-    expect(calcFuelForModule(12)).toBe(2)
+    expect(calcFuel(12)).toBe(2)
   })
 
   it('of mass 14 should be 2', () => {
-    expect(calcFuelForModule(14)).toBe(2)
+    expect(calcFuel(14)).toBe(2)
   })
 
   it('of mass 1969 should be 654', () => {
-    expect(calcFuelForModule(1969)).toBe(654)
+    expect(calcFuel(1969)).toBe(654)
   })
 
   it('of mass 100756 should be 33583', () => {
-    expect(calcFuelForModule(100756)).toBe(33583)
+    expect(calcFuel(100756)).toBe(33583)
   })
 })
 
 describe('the sum of the fuel for a space craft', () => {
   it('with modules of mass 12 should be 2', () => {
     const modules = [12]
-    expect(calcSumOfFuelRequirements(modules)).toBe(2)
+    expect(calcSumOfFuelRequirements(modules, calcFuel)).toBe(2)
   })
 
   it('with modules of mass 12, 14, 20 should be 8', () => {
     const modules = [12, 14, 20]
-    expect(calcSumOfFuelRequirements(modules)).toBe(8)
+    expect(calcSumOfFuelRequirements(modules, calcFuel)).toBe(8)
   })
 })
+
+describe('the fuel for a module (including its fuel)', () => {
+  it('of mass 12 should be 2', () => {
+    expect(calcFuelIncludingItsFuel(12)).toBe(2)
+  })
+
+  it('of mass 1969 should be 966', () => {
+    expect(calcFuelIncludingItsFuel(1969)).toBe(966)
+  })
+
+  it('of mass 100756 should be 33583', () => {
+    expect(calcFuelIncludingItsFuel(100756)).toBe(50346)
+  })
+})
+
+describe('the sum of the fuel for a space craft (including its fuel)', () => {
+  it('with modules of mass 12 should be 2', () => {
+    const modules = [12]
+    expect(calcSumOfFuelRequirements(modules, calcFuelIncludingItsFuel)).toBe(2)
+  })
+
+  it('with modules of mass 12, 14, 20 should be 8', () => {
+    const modules = [12, 14, 20]
+    expect(calcSumOfFuelRequirements(modules, calcFuelIncludingItsFuel)).toBe(8)
+  })
+})
+
